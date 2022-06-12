545cc25a4e336a70ad3fb404cf3514b5a7771e62
99 <hahahoho5915@gmail.com>
2022-06-12 19:39:29 +0900

    기능추가 - StudyController 추가

---
+++ AspNetCoreWebStudy/Controllers/StudyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebStudy.Controllers
+{
+    public class StudyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+
+        public IActionResult CSharp()
+        {
+            return View();
+        }
+
+        public IActionResult Cpp()
+        {
+            return View();
+        }
+    }
+}
