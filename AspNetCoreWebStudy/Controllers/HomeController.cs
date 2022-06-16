be61083a9351780bb766c498540829c4f5e0510f
99 <hahahoho5915@gmail.com>
2022-06-16 21:36:49 +0900

    [학습] MVC 학습
    - Model을 View에 넘기는 3가지 방법
    1. return View(object);
    2. ViewBag.NAME = object;
    3. ViewDta["NAME"] = int or string // object는 못넣음

--- AspNetCoreWebStudy/Controllers/HomeController.cs
+++ AspNetCoreWebStudy/Controllers/HomeController.cs
@@ -15,6 +15,21 @@ namespace AspNetCoreWebStudy.Controllers
 
         public IActionResult Index()
         {
+            var honeUser = new User
+            {
+                UserNo = 1,
+                UserName = "홍길동"
+            };
+
+            // 1번째 방식
+            // return View(honeUser);
+
+            // 2번째 방식
+            // ViewBag.User = honeUser;
+
+            // 3번째 방식(객체는 못넣음)
+            ViewData["UserNo"] = honeUser.UserNo;
+            ViewData["UserName"] = honeUser.UserName;
             return View();
         }
 
---
+++ AspNetCoreWebStudy/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreWebStudy.Models
+{
+    public class User
+    {
+        // 사용자 번호
+        public int UserNo { get; set; }
+        
+        // 사용자 이름
+        public string UserName { get; set; }
+    }
+}
